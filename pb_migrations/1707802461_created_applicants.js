6ef89d3d310b5bd1c971b6cae1a9a45773e23b3c
Ann <59127075+kotogavr@users.noreply.github.com>
2024-02-18 20:02:03 +0800

    proper migrations

--- pb_migrations/1707802461_created_applicants.js
+++ pb_migrations/1707802461_created_applicants.js
@@ -1,17 +1,30 @@
 /// <reference path="../pb_data/types.d.ts" />
 migrate((db) => {
   const collection = new Collection({
-    "id": "voe17td77nnrfve",
-    "created": "2024-01-30 07:16:06.703Z",
-    "updated": "2024-01-30 07:16:06.703Z",
-    "name": "tools",
+    "id": "wvgmixqca5qkcjh",
+    "created": "2024-02-13 05:34:21.098Z",
+    "updated": "2024-02-13 05:34:21.098Z",
+    "name": "applicants",
     "type": "base",
     "system": false,
     "schema": [
       {
         "system": false,
-        "id": "jkpstdle",
-        "name": "name",
+        "id": "kybv9m9e",
+        "name": "email",
+        "type": "email",
+        "required": true,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "exceptDomains": [],
+          "onlyDomains": []
+        }
+      },
+      {
+        "system": false,
+        "id": "o01njpr1",
+        "name": "bio",
         "type": "text",
         "required": false,
         "presentable": false,
@@ -23,7 +36,9 @@ migrate((db) => {
         }
       }
     ],
-    "indexes": [],
+    "indexes": [
+      "CREATE UNIQUE INDEX `idx_CfDERWO` ON `applicants` (`email`)"
+    ],
     "listRule": null,
     "viewRule": null,
     "createRule": null,
@@ -35,7 +50,7 @@ migrate((db) => {
   return Dao(db).saveCollection(collection);
 }, (db) => {
   const dao = new Dao(db);
-  const collection = dao.findCollectionByNameOrId("voe17td77nnrfve");
+  const collection = dao.findCollectionByNameOrId("wvgmixqca5qkcjh");
 
   return dao.deleteCollection(collection);
 })
